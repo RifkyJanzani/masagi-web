fd88016d78ff95f3c2b8c61c3e5c273e77881223
farhan <farhanangga8900@gmail.com>
2025-07-10 21:38:21 +0700

    hapus sidebar konten

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -32,15 +32,15 @@ export default function Sidebar() {
         </svg>
       ),
     },
-    {
-      title: 'Konten',
-      path: '/admin/konten',
-      icon: (
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z" />
-        </svg>
-      ),
-    },
+    // {
+    //   title: 'Konten',
+    //   path: '/admin/konten',
+    //   icon: (
+    //     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    //       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z" />
+    //     </svg>
+    //   ),
+    // },
     {
       title: 'Jurnal',
       path: '/admin/jurnal',
